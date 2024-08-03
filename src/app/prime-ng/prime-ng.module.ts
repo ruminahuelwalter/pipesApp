6e87edfd612716d0b81364834e5ebaab716da86a
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-03 20:26:14 -0300

    uppercase, lowercase, titlecase

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    MenubarModule,
     MenuModule,
-    MenubarModule
+    PanelModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-basics-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    PrimeNgModule
+  ],
   templateUrl: './basics-page.component.html',
   styleUrl: './basics-page.component.css'
 })
 export class BasicsPageComponent {
+  public nameLower: string = 'walter';
+  public nameUpper: string = 'WALTER';
+  public fullName: string = 'wAlTeR';
 
 }
