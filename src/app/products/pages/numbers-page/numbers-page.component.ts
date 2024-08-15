4447e3d7331140be3d53e240d29bb03ba4abc14b
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-15 19:21:11 -0300

    decimal, currency y percent pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
 
 @Component({
   selector: 'app-numbers-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    PrimeNgModule
+  ],
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
 
+  public totalSells: number = 2567789.557
+  public percent: number = 0.4856
 }
