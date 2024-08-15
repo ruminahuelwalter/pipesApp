c014c1802bbf68a24a0af208d462a24c7c787f7e
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-15 19:48:10 -0300

    fieldset

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
 
 @Component({
   selector: 'app-uncommon-page',
   standalone: true,
-  imports: [],
+  imports: [
+    FieldsetModule,
+    PrimeNgModule
+  ],
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css'
 })
