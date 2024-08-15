3558d61b889aa13efe6e0a66e5e90475e58e04c4
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-15 20:37:00 -0300

    i18Select Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
+import { I18nSelectPipe } from '@angular/common';
 
 @Component({
   selector: 'app-uncommon-page',
   standalone: true,
   imports: [
     FieldsetModule,
-    PrimeNgModule
+    PrimeNgModule,
+    I18nSelectPipe
   ],
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+   // i18Select
+  public name: string = 'Walter';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
 
+  changeClient():void {
+    this.name = 'Melissa';
+    this.gender= 'female'
+   }
 }
