2b76d98a42e2399750f651bbe4b7370a0c0a6162
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-16 20:24:04 -0300

    order component

---
+++ src/app/products/pages/order/order.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
+
+@Component({
+  selector: 'products-order',
+  standalone: true,
+  imports: [
+    PrimeNgModule
+  ],
+  templateUrl: './order.component.html',
+  styles: ``
+})
+export class OrderComponent {
+
+}
