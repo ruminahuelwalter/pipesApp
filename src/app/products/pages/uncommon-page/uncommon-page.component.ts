f8c101e94de7f0a41fa05a51759a0fdb5a650441
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-08-15 21:12:25 -0300

    i18nPluralPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nSelectPipe, I18nPluralPipe } from '@angular/common';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -9,7 +9,8 @@ import { I18nSelectPipe } from '@angular/common';
   imports: [
     FieldsetModule,
     PrimeNgModule,
-    I18nSelectPipe
+    I18nSelectPipe,
+    I18nPluralPipe
   ],
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css'
@@ -26,5 +27,19 @@ export class UncommonPageComponent {
   changeClient():void {
     this.name = 'Melissa';
     this.gender= 'female'
-   }
+  }
+
+  // i18nPlural
+  public clients: string[]= ['Walter', 'Emanuel', 'Maria', 'Azul'];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos 1 cliente esperando',
+    '=2': 'tenemos 2 personas esperando',
+    'other': 'tenemos # clientes esperando',
+  }
+
+  public deleteClient(): void{
+    this.clients.shift();
+  }
 }
